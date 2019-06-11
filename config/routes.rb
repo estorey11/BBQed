08d8d06caa6cf8614579ce55ff37b17af72cdae0
github username <github email address>
2019-06-11 11:48:44 -0400

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :recipes
 
   root 'welcome#home'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
