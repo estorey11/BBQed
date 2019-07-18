fcc98aeb614b2505441bbef814dd8e2892ec6fd4
github username <github email address>
2019-07-18 19:52:15 -0400

    Add js to user show page

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :food_unit, :food_amount, :temp, :wood, :time, :result, :rub, :wrap, :results_image
+  attributes :id, :food_unit, :food_amount, :temp, :wood, :time, :result, :rub, :wrap, :results_image, :food
   belongs_to :user
   belongs_to :food
   belongs_to :smoker
