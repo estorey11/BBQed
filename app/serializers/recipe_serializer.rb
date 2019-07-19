96d351e2608dc51cdba53fff452928038b4fd02c
github username <github email address>
2019-07-18 20:39:42 -0400

    Add full recipe buttons to user show page

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,9 @@
 class RecipeSerializer < ActiveModel::Serializer
+
   attributes :id, :food_unit, :food_amount, :temp, :wood, :time, :result, :rub, :wrap, :results_image, :food
+
   belongs_to :user
   belongs_to :food
   belongs_to :smoker
+
 end
